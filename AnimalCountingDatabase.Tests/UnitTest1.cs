96b07f6246a4a84cd3c3a5e275f602c649028c74
xgrois <xgrois@gmail.com>
2022-03-02 11:47:02 +0100

    Add Tests

---
+++ AnimalCountingDatabase.Tests/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Xunit;
+
+namespace AnimalCountingDatabase.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+            Assert.True(1 == 1);
+        }
+    }
+}
